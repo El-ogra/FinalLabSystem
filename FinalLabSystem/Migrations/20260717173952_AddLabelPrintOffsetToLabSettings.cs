da55b00ecaa51ec928146048869b2efd26cb483c
medowemado <medowemado@gmail.com>
2026-07-17 20:51:18 +0300

    Add migration: AddLabelPrintOffsetToLabSettings

---
+++ FinalLabSystem/Migrations/20260717173952_AddLabelPrintOffsetToLabSettings.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalLabSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLabelPrintOffsetToLabSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "LabelPrintOffsetXmm",
+                table: "LabSettings",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "LabelPrintOffsetYmm",
+                table: "LabSettings",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LabelPrintOffsetXmm",
+                table: "LabSettings");
+
+            migrationBuilder.DropColumn(
+                name: "LabelPrintOffsetYmm",
+                table: "LabSettings");
+        }
+    }
+}
